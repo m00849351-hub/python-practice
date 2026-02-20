4b272e13005c28e1da17b065b8de2afa7212f6a6
m00849351-hub <m00849351@gmail.com>
2026-02-20 11:58:35 +0530

    Add files via upload

---
+++ class_std.py
@@ -0,0 +1,11 @@
+class student:
+    def __init__(self, name, marks):
+       self.name = name
+       self.marks = marks
+
+    def show(self):
+        print("Name: ", self.name)
+        print("Marks: ", self.marks)
+
+s1 = student("Mahi", 90)
+s1.show()
