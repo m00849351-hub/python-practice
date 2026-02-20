ba4121155948e369b786c50bed542ed881da8a00
m00849351-hub <m00849351@gmail.com>
2026-02-20 11:58:01 +0530

    Add files via upload

---
+++ lists/search_name.py
@@ -0,0 +1,8 @@
+names = ["Mahi", "Aman", "Riya", "Yash", "Reyaan", "Savi", "Sonia"]
+
+search = input("Enter name to search: ")
+
+if search in names:
+    print("Found")
+else:
+    print("Not found")
